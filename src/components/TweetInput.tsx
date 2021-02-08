3732f96368566cb6ffd70ba4a203efa2076d3727
Omitsu <a826.orhrp@gmail.com>
2021-02-08 14:45:15 +0900

    🎉

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./TweetInput.module.css";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
-import { auth } from "../firebase";
-import { Avatar } from "@material-ui/core";
+import { storage, db, auth } from "../firebase";
+import { Avatar, Button, IconButton } from "@material-ui/core";
+import firebase from "firebase/app";
+import AddPhotoIcon from "@material-ui/icons/AddAPhoto";
 
 const TweetInput = () => {
   const user = useSelector(selectUser);
+  const [tweetImage, setTweetImage] = useState<File | null>(null);
+  const [tweetMsg, setTweetMsg] = useState("");
+  const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setTweetImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
+  const sendTweet = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (tweetImage) {
+    } else {
+      db.collection("posts").add({
+        avatar: user.photoUrl,
+        image: "",
+        text: tweetMsg,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        username: user.displayName,
+      });
+    }
+    setTweetImage(null);
+    setTweetMsg("");
+  };
+
   return (
     <div>
       <Avatar
