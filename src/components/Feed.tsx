7f2a86a7d0bcb00532a56fac48b564b8c625c8c7
Omitsu <a826.orhrp@gmail.com>
2021-02-13 09:38:44 +0900

    ツイートをフィードに表示するための処理

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Feed.module.css";
-import { auth } from "../firebase";
+import { db } from "../firebase";
 import TweetInput from "./TweetInput";
 
-const Feed = () => {
+const Feed: React.FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: "",
+      avatar: "",
+      image: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        )
+      );
+    return () => {
+      unSub();
+    };
+  }, []);
   return (
     <div className={styles.feed}>
-      Feed
       <TweetInput />
-      <button onClick={() => auth.signOut()}>Logout</button>
+      {posts.map((post) => (
+        <h3>{post.id}</h3>
+      ))}
+      {posts.map((post) => (
+        <h3>{post.text}</h3>
+      ))}
     </div>
   );
 };
--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -7,7 +7,7 @@ import { selectUser } from "../features/userSlice";
 import { Avatar, Button, IconButton } from "@material-ui/core";
 import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 
-function TweetInput() {
+const TweetInput: React.FC = () => {
   const user = useSelector(selectUser);
   const [tweetImage, setTweetImage] = useState<File | null>(null);
   const [tweetMsg, setTweetMsg] = useState("");
@@ -111,6 +111,6 @@ function TweetInput() {
       </form>
     </>
   );
-}
+};
 
 export default TweetInput;
